1e16e54431871628211ecb925f575841364d8ace
Nicolab.net <dev@nicolab.net>
2016-01-14 18:11:16 +0100

    [config] add setter of env. Add freezeEnv

--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@
 let path = require('path');
 
 let cfg = {
+  freezeEnv : false,
   pkg       : require(path.join(path.resolve(), 'package.json')),
   srcDir    : 'src',
   publicDir : 'public',
@@ -26,6 +27,18 @@ let cfg = {
   localPort : process.env.SERVE_PORT || 8080,
   get env() {
     return process.env.NODE_ENV || require('./').gp.util.env.type;
+  },
+  set env(value) {
+    if(this.freezeEnv) {
+      throw new Error(
+        'Cannot replace the "env" value. ' +
+        'Because the "env" property is configured in read only. ' +
+        '(see flow.cfg.freezeEnv).'
+      );
+    }
+
+    process.env.NODE_ENV = value;
+    require('./').gp.util.env.type = value;
   },
   get banner() {
     let pkg = this.pkg;
